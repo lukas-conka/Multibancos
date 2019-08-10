1a9c7d23fb06bb20712480e79bee305a36fbd8e3
Lukas <lukas@hotmail.co.jp>
2019-08-10 03:26:04 -0300

    Exemplo com Sequelize e Postgres

---
+++ src/postgresExample.js
@@ -0,0 +1,46 @@
+//npm install sequelize pg-hstore pg 
+const Sequelize = require('sequelize')
+const driver = new Sequelize(
+    'heroes',
+    'lucasamaral',
+    'swordfish@7',
+    {
+        host: 'localhost',
+        dialect: 'postgres',
+        quoteIdentifiers: false,
+        //operatorsAliases: true
+
+    }
+)
+
+async function main() {
+    const Herois = driver.define('herois', {
+        id:{
+            type: Sequelize.INTEGER,
+            required: true,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        nome: {
+            type: Sequelize.STRING,
+            require: true
+        },
+        poder: {
+            type: Sequelize.STRING,
+            required: true
+        }
+    }, {
+        tableName: 'TB_HEROIS',
+        freezeTableName: false, 
+        timestamps: false
+    })
+
+    await Herois.sync()
+
+    const result = await Herois.findAll({
+        raw:true
+    })
+    console.log('result', result)
+}
+
+main()
